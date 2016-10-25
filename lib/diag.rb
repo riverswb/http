5104c0069812d451a535e8990525d696f6e67d31
Brett Rivers <riverswb@gmail.com>
2016-10-24 20:34:12 -0600

    me thinks iteration 1 is done

--- lib/diag.rb
+++ lib/diag.rb
@@ -1,31 +1,31 @@
 class Diag
 
   def output_message_verb(input)
-    "VERB: #{input[0].split("/")[0].rstrip}"
+    "VERB: #{input[0].split("/")[0].rstrip}\n"
   end #=> Verb: GET
 
   def output_message_protocol(input)
-    "Protocol: #{input[0].split(" ")[-1]}"
+    "Protocol: #{input[0].split(" ")[-1]}\n"
   end #=> Protocol: HTTP/1.1
 
   def output_message_host(input)
-    "Host: #{input[1].split(":")[1].lstrip}"
+    "Host: #{input[1].split(":")[1].lstrip}\n"
   end #=> Host: 127.0.0.1
 
   def output_message_port(input)
-    "Port: #{input[1].split(":")[-1]}"
+    "Port: #{input[1].split(":")[-1]}\n"
   end #=> Port: 9292
 
   def output_message_origin(input)
-    "Origin: #{input[1].split(":")[1].lstrip}"
+    "Origin: #{input[1].split(":")[1].lstrip}\n"
   end #=> Origin: 127.0.0.1
 
   def output_message_accept(input)
-    "#{input[6]}"
+    "#{input[6]}\n"
   end #=> Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
 
   def output_message_path(input)
-    "Path: /"
+    "Path: /\n"
   end #=> Path: /
 
 end
--- lib/http.rb
+++ lib/http.rb
@@ -2,11 +2,11 @@ require './lib/diag'
 require 'socket'
 
 class Http
-  attr_accessor :d
 
-  def initialize
-    @d = Diag.new
-  end
+  #
+  # def d
+  #   Diag.new
+  # end
 
   tcp_server = TCPServer.new(9292)
   client = tcp_server.accept
@@ -23,12 +23,17 @@ class Http
   puts request_lines.inspect
 
   puts "Sending response"
-  binding.pry
+  # binding.pry
   response = "<pre>" + request_lines.join("\n") + "</pre>"
-  body = "<pre>\n" + "#{d.output_message_verb(request_lines)}" + "Path: / \n" "Protocol: HTTP/1.1\n"+
-    "#{request_lines[1]}\n"+ "Port: 9292\n" + "Origin: 127.0.0.1\n" +
-    "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
-    + "</pre>"
+  body = "<pre>\n" +
+      Diag.new.output_message_verb(request_lines) +
+      Diag.new.output_message_path(request_lines) +
+      Diag.new.output_message_protocol(request_lines) +
+      Diag.new.output_message_host(request_lines) +
+      Diag.new.output_message_port(request_lines) +
+      Diag.new.output_message_origin(request_lines) +
+      Diag.new.output_message_accept(request_lines) +
+      "</pre>"
   output = "<html><head></head><body>#{response}</body></html>"
   headers = ["http/1.1 200 ok",
             "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
