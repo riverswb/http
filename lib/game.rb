3daaae1f2df11b85ebf55f89e071df8537fbb862
Brett Rivers <riverswb@gmail.com>
2016-10-26 06:28:35 -0600

    game file is set up

---
+++ lib/game.rb
@@ -0,0 +1,32 @@
+class Game
+  attr_reader :guess_count, :last_guess, :number
+  def initialize
+    @guess_count = 0
+    @last_guess = 0
+    @number = number
+  end
+
+  def number_generator
+    @number = (0..100).to_a.sample
+  end
+
+  def set_number(input)
+    @number = input
+  end
+
+  def make_guess(guess)
+    @guess_count += 1
+    @last_guess = guess
+    "You guessed #{guess}"
+  end
+
+  def feedback
+    if last_guess > number
+      "Last guess was too high"
+    elsif last_guess < number
+      "Last guess was too low"
+    elsif last_guess == number
+      "That is correct"
+    end
+  end
+end
