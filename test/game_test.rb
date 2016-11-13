7d3f2b1d76adacd081f4db9aea761041a9ed497b
Brett Rivers <riverswb@gmail.com>
2016-11-13 12:36:30 -0700

    some more work on game

--- test/game_test.rb
+++ test/game_test.rb
@@ -3,10 +3,11 @@ require './lib/game'
 require './lib/http'
 
 class GameTest < Minitest::Test
-  attr_reader :start_game, :http
+  attr_reader :start_game, :http, :game
   def setup
+    @game = Game.new
     @http = Http.new
-    input = ["POST /start_game HTTP/1.1",
+    start_input = ["POST /start_game HTTP/1.1",
             "Host: 127.0.0.1:9292",
             "Connection: keep-alive",
             "Cache-Control: no-cache",
@@ -18,21 +19,22 @@ class GameTest < Minitest::Test
             "Accept-Encoding: gzip, deflate, sdch, br",
             "Accept-Language: en-US,en;q=0.8"]
 
-    @start_game = http.check_verb(input)
+    @start_game = http.check_verb(start_input)
   end
 
   def test_game_exists
-    assert_instance_of Game, Game.new
+    assert_instance_of Game, game
   end
 
-  def test_when_starting_a_game_a_number_is_picked
+  def test_when_starting_a_game_a_number_is_picked_between_0_and_100
     game = Game.new
 
-    assert_equal true, (0..100).include?(game.number)
+    assert (0..100).include?(game.number)
+    assert (-1 < game.number)
+    assert (101 > game.number)
   end
 
   def test_when_path_is_start_game_it_prints_good_luck
-    http = Http.new
     input = ["POST /start_game HTTP/1.1",
             "Host: 127.0.0.1:9292",
             "Connection: keep-alive",
@@ -44,6 +46,7 @@ class GameTest < Minitest::Test
             "Accept: */*",
             "Accept-Encoding: gzip, deflate, sdch, br",
             "Accept-Language: en-US,en;q=0.8"]
+
     output = "Good Luck!"
     assert_equal output, http.check_verb(input)
   end
@@ -81,5 +84,26 @@ class GameTest < Minitest::Test
 
     output = "Check your verb, please"
     assert_equal output, http.check_verb(input)
+  end
+
+  def test_hint_tells_you_if_it_is_too_low
+    start_game
+    game.set_guess(-1)
+
+    output = "Too low"
+    assert_equal output, game.hint
+  end
+
+  def test_hint_tells_you_if_it_is_too_high
+    start_game
+    game.set_guess(101)
+
+    output = "Too high"
+    assert_equal output, game.hint
+  end
+
+  def test_if_guess_is_not_a_number_it_directs_you
+    output = "Please make guesses as a whole number, between 0 and 100"
+    assert_equal output, game.set_guess("pizza")
   end
 end
